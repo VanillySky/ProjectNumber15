a6bfaca60d83df87ea1a85e1216417a3f386c4e1
VanilluSky <vikame94@gmail.com>
2021-05-30 18:09:35 +0300

    BuildNewQuestion

---
+++ Project/gui/BuildNewQuestionController.java
@@ -0,0 +1,49 @@
+package gui;
+    import javafx.fxml.FXML;
+	import javafx.scene.control.Button;
+    import javafx.scene.control.TextField;
+
+	public class BuildNewQuestionController {
+
+	    @FXML
+	    private Button MainButton;
+
+	    @FXML
+	    private Button SignOutButton;
+
+	    @FXML
+	    private Button ReturnButton;
+
+	    @FXML
+	    private Button AddQuestionButton;
+
+	    @FXML
+	    private TextField RightAnswerTxt;
+
+	    @FXML
+	    private TextField CourseTxt;
+
+	    @FXML
+	    private TextField Answer1Txt;
+
+	    @FXML
+	    private TextField QuestionNumberTxt;
+
+	    @FXML
+	    private TextField Answer3Txt;
+
+	    @FXML
+	    private TextField Answer4Txt;
+
+	    @FXML
+	    private TextField QustionInstructionTxt;
+
+	    @FXML
+	    private TextField TheQuestionTxt;
+
+	    @FXML
+	    private TextField Answer2Txt;
+
+	}
+
+
