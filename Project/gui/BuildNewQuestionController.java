857f725a5f546169d696076dc0e65eb2b10745ed
Rostislav93 <galburt.r@gmail.com>
2021-05-31 16:27:44 +0300

    C

--- Project/gui/BuildNewQuestionController.java
+++ Project/gui/BuildNewQuestionController.java
@@ -1,49 +1,177 @@
 package gui;
-    import javafx.fxml.FXML;
-	import javafx.scene.control.Button;
-    import javafx.scene.control.TextField;
 
-	public class BuildNewQuestionController {
+import java.io.IOException;
+import java.util.ArrayList;
 
-	    @FXML
-	    private Button MainButton;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
-	    @FXML
-	    private Button SignOutButton;
+public class BuildNewQuestionController {
 
-	    @FXML
-	    private Button ReturnButton;
+	@FXML
+	private Button MainButton;
 
-	    @FXML
-	    private Button AddQuestionButton;
+	@FXML
+	private Button SignOutButton;
 
-	    @FXML
-	    private TextField RightAnswerTxt;
+	@FXML
+	private Button ReturnButton;
 
-	    @FXML
-	    private TextField CourseTxt;
+	@FXML
+	private Button AddQuestionButton;
 
-	    @FXML
-	    private TextField Answer1Txt;
+	@FXML
+	private TextField RightAnswerTxt;
 
-	    @FXML
-	    private TextField QuestionNumberTxt;
+	@FXML
+	private TextField CourseTxt;
 
-	    @FXML
-	    private TextField Answer3Txt;
+	@FXML
+	private TextField Answer1Txt;
 
-	    @FXML
-	    private TextField Answer4Txt;
+	@FXML
+	private TextField QuestionNumberTxt;
 
-	    @FXML
-	    private TextField QustionInstructionTxt;
+	@FXML
+	private TextField Answer3Txt;
 
-	    @FXML
-	    private TextField TheQuestionTxt;
+	@FXML
+	private TextField Answer4Txt;
 
-	    @FXML
-	    private TextField Answer2Txt;
+	@FXML
+	private TextField QustionInstructionTxt;
 
+	@FXML
+	private TextField TheQuestionTxt;
+
+	@FXML
+	private TextField Answer2Txt;
+
+	@FXML
+	// DatabaseHandler dbHandler
+	public ArrayList<String> AL = new ArrayList();
+	//private Client cl = new Client();
+
+	public void AddQuestionButton() {
+		
+		if(CourseTxt.getText().equals("")||QuestionNumberTxt.getText().equals("")||
+				TheQuestionTxt.equals("")||Answer1Txt.getText().equals("")||
+				Answer2Txt.getText().equals("")||Answer3Txt.getText().equals("")||Answer4Txt.getText().equals("")
+				||RightAnswerTxt.getText().equals("")||
+		QustionInstructionTxt.getText().equals(""))
+		{
+			
+			CourseTxt.setText("Error");
+			return;
+		}
+		
+		AddQuestionButton.setOnAction(event -> {
+			AL.add(CourseTxt.getText());
+			AL.add(QuestionNumberTxt.getText());
+			AL.add(TheQuestionTxt.getText());
+			AL.add(Answer1Txt.getText());
+			AL.add(Answer2Txt.getText());
+			AL.add(Answer3Txt.getText());
+			AL.add(Answer4Txt.getText());
+			AL.add(RightAnswerTxt.getText());
+			AL.add(QustionInstructionTxt.getText());
+
+			//cl.(AL,"addQuestion");
+
+			AddQuestionButton.getScene().getWindow().hide();
+
+			FXMLLoader loader = new FXMLLoader();
+
+			loader.setLocation(getClass().getResource("/gui/BuildQuestion.fxml"));
+
+			try {
+				loader.load();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			Parent root = loader.getRoot();
+			Stage stage = new Stage();
+			stage.setScene(new Scene(root));
+			stage.showAndWait();
+
+		});
+		
 	}
 
+	@FXML
+	public void PressCEMS() {
+		MainButton.setOnAction(event -> {
+			MainButton.getScene().getWindow().hide();
 
+			FXMLLoader loader = new FXMLLoader();
+
+			loader.setLocation(getClass().getResource("/gui/TeacherMain.fxml"));
+
+			try {
+				loader.load();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			Parent root = loader.getRoot();
+			Stage stage = new Stage();
+			stage.setScene(new Scene(root));
+			stage.showAndWait();
+		});
+	}
+
+	@FXML
+	public void PressReturnButton() {
+		ReturnButton.setOnAction(event -> {
+			ReturnButton.getScene().getWindow().hide();
+
+			FXMLLoader loader = new FXMLLoader();
+
+			loader.setLocation(getClass().getResource("/gui/BuildQuestions.fxml"));
+
+			try {
+				loader.load();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			Parent root = loader.getRoot();
+			Stage stage = new Stage();
+			stage.setScene(new Scene(root));
+			stage.showAndWait();
+		});
+	}
+
+	@FXML
+	public void SignOut() {
+		SignOutButton.setOnAction(event -> {
+			SignOutButton.getScene().getWindow().hide();
+
+			FXMLLoader loader = new FXMLLoader();
+			///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+			loader.setLocation(getClass().getResource("/gui/Signin.fxml"));
+			///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+			try {
+				loader.load();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			Parent root = loader.getRoot();
+			Stage stage = new Stage();
+			stage.setScene(new Scene(root));
+			stage.showAndWait();
+		});
+	}
+
+}
--- Project/gui/BuildQuestionsController.java
+++ Project/gui/BuildQuestionsController.java
@@ -1,10 +1,16 @@
 package gui;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class BuildQuestionsController {
 
@@ -61,5 +67,53 @@ public class BuildQuestionsController {
 
     @FXML
     private TextField teacherNameTXT;
+    
+    @FXML
+    public void PressCEMS() {
+   	 CEMSButton.setOnAction(event -> {
+   		 CEMSButton.getScene().getWindow().hide();
+
+   			FXMLLoader loader = new FXMLLoader();
+
+   			loader.setLocation(getClass().getResource("/gui/TeacherMain.fxml"));
+
+   			try {
+   				loader.load();
+   			} catch (IOException e) {
+   				// TODO Auto-generated catch block
+   				e.printStackTrace();
+   			}
+
+   			Parent root = loader.getRoot();
+   			Stage stage = new Stage();
+   			stage.setScene(new Scene(root));
+   			stage.showAndWait();
+   		});
+    }
+    
+ 
+    
+    @FXML
+    public void SignOut() {
+    	SignOutButton.setOnAction(event -> {
+    		SignOutButton.getScene().getWindow().hide();
+
+      			FXMLLoader loader = new FXMLLoader();
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      			loader.setLocation(getClass().getResource("/gui/Signin.fxml"));
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      			try {
+      				loader.load();
+      			} catch (IOException e) {
+      				// TODO Auto-generated catch block
+      				e.printStackTrace();
+      			}
+
+      			Parent root = loader.getRoot();
+      			Stage stage = new Stage();
+      			stage.setScene(new Scene(root));
+      			stage.showAndWait();
+      		});
+    }
 
 }
